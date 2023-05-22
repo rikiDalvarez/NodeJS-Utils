3f60e372d7a75148116cba1e3180f5fc472c1d17
Ricardo <ric.dalvarez@hotmail.com>
2023-05-22 13:15:05 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Manipulating files with NodeJS #
+
+this step by step assume that you have node ~v19 installed
+
+After cloning the repo on you local machine navegate to terminal and go to the folder path.
+
+After that follow the stream of the functinos from top to bottom.
+Execute ```javascript
+writeFile(string, path)```
+
+if the path param is omitted a file with the current year will be created.
--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 
 //n1e1
-// const writeFile = require("./writeFile")
-// writeFile("break line")
+const writeFile = require("./writeFile")
+writeFile("log")
 
 // //n1e2
 // const readFile = require("./readFile")
 // console.log(readFile());
 
-//n1e3
+// //n1e3
 // const compressFile = require("./compressFile")
 // compressFile("./2023.txt",)
 
-//n2e1
+// //n2e1
 // const printRecursive = require("./printRecursive");
 // printRecursive()
 
-//n2e2
+// //n2e2
 // const listContent = require("./listContent")
 // listContent()
 
-//n3
-// codificar archivo a hex y base64
-const createCodFileHexBase = require("./createCodFileHexBase")
-createCodFileHexBase("./2023.txt")
+// //n3
+// // codificar archivo a hex y base64
+// const createCodFileHexBase = require("./createCodFileHexBase")
+// createCodFileHexBase("./2023.txt")
 
-// encrypt file with algo aes-192-cbc and delete inicial file
-const crypto = require("crypto")
-const password = "0000"
-const iv = Buffer.alloc(16, 0)
-const key = crypto.scryptSync(password, "salt", 24);
-const fs = require("fs")
-const encryptFiles = require("./encryptFiles")
+// // Variables
+// const crypto = require("crypto")
+// const password = "0000"
+// const iv = Buffer.alloc(16, 0)
+// const key = crypto.scryptSync(password, "salt", 24);
+// const fs = require("fs")
+// const encryptFiles = require("./encryptFiles")
 
-const encryptFilesAndDelete = async (filename1, filename2, key, iv) => {
-	try {
-		await encryptFiles(filename1, key, iv);
-		fs.unlinkSync(filename1);
+// // encrypt file with algo aes-192-cbc and delete inicial file
+// const encryptFilesAndDelete = async (filename1, filename2, key, iv) => {
+// 	try {
+// 		await encryptFiles(filename1, key, iv);
+// 		fs.unlinkSync(filename1);
 
-		await encryptFiles(filename2, key, iv);
-		fs.unlinkSync(filename2);
+// 		await encryptFiles(filename2, key, iv);
+// 		fs.unlinkSync(filename2);
 
-	} catch (error) {
-		console.log(error)
-	}
-}
+// 	} catch (error) {
+// 		console.log(error)
+// 	}
+// }
+// encryptFilesAndDelete("./2023_hex.txt", "./2023_base64.txt", key, iv);
 
-encryptFilesAndDelete("./2023_hex.txt", "./2023_base64.txt", key, iv);
-
-//DECRYPT FILE
-const decryptFile = require("./decryptFile")
-decryptFile("2023_hex_encrypted.txt", "2023_base64_encrypted.txt", key, iv)
+// // decrypt and decode files
+// const decryptFile = require("./decryptFile")
+// decryptFile("2023_hex_encrypted.txt", "2023_base64_encrypted.txt", key, iv)
